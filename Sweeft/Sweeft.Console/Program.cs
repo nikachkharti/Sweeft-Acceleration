dc65a59b0c97d85886bf9c8716b1e9aaa6b1f8bb
nikachkharti <nchkhartishvili@novasystems.ge>
2023-03-17 11:10:39 +0400

    First 6 tasks completed

---
+++ Sweeft/Sweeft.Console/Program.cs
@@ -0,0 +1,122 @@
+﻿
+//6.დავალების სკრიპტი იხილეთ School.Sql ფაილში...
+
+
+//5.გვაქვს n სართულიანი კიბე, ერთ მოქმედებაში შეგვიძლია ავიდეთ 1 ან 2 საფეხურით. დაწერეთ ფუნქცია რომელიც დაითვლის n სართულზე ასვლის ვარიანტების რაოდენობას.
+int CountVariants(int stairCount)
+{
+    int variants = 0;
+
+    bool oneWayVariant = false;
+    bool twoWayVariant = true;
+
+    while (stairCount != 0)
+    {
+        if (oneWayVariant)
+        {
+            stairCount--;
+            variants++;
+        }
+
+        if (twoWayVariant) 
+        {
+            stairCount -= 2;
+            variants++;
+        }
+    }
+
+    return variants;
+}
+
+
+//4.მოცემულია String რომელიც შედგება „(„ და „)“ ელემენტებისგან.დაწერეთ ფუნქცია რომელიც აბრუნებს ფრჩხილები არის თუ არა მათემატიკურად სწორად დასმული. მაგ: (()()) სწორი მიმდევრობაა,  ())() არასწორია
+bool IsProperly(string sequence)
+{
+    Stack<char> stack = new Stack<char>();
+    foreach (char c in sequence)
+    {
+        if (c == '(')
+        {
+            stack.Push(c);
+        }
+        else if (c == ')')
+        {
+            if (stack.Count == 0)
+            {
+                return false;
+            }
+            char lastElement = stack.Pop();
+            if (lastElement != '(')
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return stack.Count == 0;
+}
+
+
+//3 მოცემულია მასივი, რომელიც შედგება მთელი რიცხვებისგან. დაწერეთ ფუნქცია რომელსაც გადაეცემა ეს მასივი და აბრუნებს მინიმალურ მთელ რიცხვს, რომელიც 0-ზე მეტია და ამ მასივში არ შედის.
+int NotContains(int[] array)
+{
+    HashSet<int> set = new(array.Where(x => x > 0));
+    int result = 1;
+    while (set.Contains(result))
+    {
+        result++;
+    }
+    return result;
+}
+
+
+//2.გვაქვს 1,5,10,20 და 50 თეთრიანი მონეტები. დაწერეთ ფუნქცია, რომელსაც გადაეცემა თანხა (თეთრებში) და აბრუნებს მონეტების მინიმალურ რაოდენობას, რომლითაც შეგვიძლია ეს თანხა დავახურდაოთ.
+int MinSplit(int amount)
+{
+    int[] coins = { 50, 20, 10, 5, 1 };
+    int count = 0;
+    for (int i = 0; i < coins.Length; i++)
+    {
+        while (amount >= coins[i])
+        {
+            amount -= coins[i];
+            count++;
+        }
+    }
+    return count;
+}
+
+
+//1.დაწერეთ ფუნქცია, რომელსაც გადაეცემა ტექსტი  და აბრუნებს პალინდრომია თუ არა. (პალინდრომი არის ტექსტი რომელიც ერთნაირად იკითხება ორივე მხრიდან). 
+bool IsPalindrome(string text)
+{
+    text = text.ToLower();
+
+    int start = 0;
+    int end = text.Length - 1;
+
+    while (start < end)
+    {
+        while (!char.IsLetterOrDigit(text[start]) && start < end)
+        {
+            start++;
+        }
+        while (!char.IsLetterOrDigit(text[end]) && start < end)
+        {
+            end--;
+        }
+
+        if (text[start] != text[end])
+        {
+            return false;
+        }
+
+        start++;
+        end--;
+    }
+
+    return true;
+}
---
+++ Sweeft/Sweeft.Console/School.Sql.sql
@@ -0,0 +1,43 @@
+﻿--6.გვაქვს Teacher ცხრილი, რომელსაც აქვს შემდეგი მახასიათებლები: სახელი, გვარი, სქესი, საგანი. გვაქვს Pupil ცხრილი, რომელსაც აქვს შემდეგი მახასიათებლები:
+--სახელი, გვარი, სქესი, კლასი. ააგეთ ნებისმიერ რელაციურ ბაზაში ისეთი დამოკიდებულება, რომელიც საშუალებას მოგვცემს,
+--რომ მასწავლებელმა ასწავლოს რამოდენიმე მოსწავლეს და ამავდროულად მოსწავლეს ჰყავდეს რამდენიმე მასწავლებელი (როგორც რეალურ ცხოვრებაში).
+
+
+--1. დაწერეთ sql რომელიც ააგებს შესაბამის table-ებს.
+CREATE DATABASE School
+GO
+
+USE School
+GO
+
+CREATE TABLE Teacher(
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	FirstName NVARCHAR(30) NOT NULL,
+	LastName NVARCHAR(40) NOT NULL,
+	Gender CHAR(1) CONSTRAINT CorrectTeacherGender CHECK(Gender = 'M' OR Gender = 'F' OR Gender = 'O') NOT NULL,
+	[Subject] NVARCHAR(50) NOT NULL
+)
+GO
+
+CREATE TABLE Pupil(
+	Id INT PRIMARY KEY IDENTITY NOT NULL,
+	FirstName NVARCHAR(30) NOT NULL,
+	LastName NVARCHAR(40) NOT NULL,
+	Gender CHAR(1) CONSTRAINT CorrectPupilGender CHECK(Gender = 'M' OR Gender = 'F' OR Gender = 'O') NOT NULL,
+	Class NVARCHAR(40) NOT NULL
+)
+GO
+
+CREATE TABLE TeacherPupil
+(
+	TeacherId INT FOREIGN KEY REFERENCES Teacher(Id),
+	PupilId INT FOREIGN KEY REFERENCES Pupil(Id),
+)
+GO
+
+--2.დაწერეთ sql რომელიც დააბრუნებს ყველა მასწავლებელს, რომელიც ასწავლის მოსწავლეს, რომელის სახელია: „გიორგი“.
+SELECT DISTINCT t.*
+FROM Teacher t
+INNER JOIN TeacherPupil tp ON t.Id = tp.TeacherID
+INNER JOIN Pupil p ON tp.PupilID = p.Id
+WHERE p.FirstName LIKE N'%გიორგი%'
